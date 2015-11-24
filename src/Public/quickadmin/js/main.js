64db57d9e9f5a38134a6e58c4e6f8a6ff35e8654
ModestasV <info@modestasv.lt>
2015-11-24 17:09:07 +0200

    Small fix. Moved to another file.

--- src/Public/quickadmin/js/main.js
+++ src/Public/quickadmin/js/main.js
@@ -3,9 +3,6 @@ $(document).ready(function () {
         "iDisplayLength": 100,
         "aaSorting": []
     });
-    $('.datepicker').datepicker({
-        autoclose: true
-    });
     $('.ckeditor').each(function () {
         CKEDITOR.replace($(this));
     })
