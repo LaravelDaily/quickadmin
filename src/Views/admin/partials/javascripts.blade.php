ce4aef9a9e31e37eed8ce2ee50471bf41b9a55a5
ModestasV <info@modestasv.lt>
2016-01-06 12:29:55 +0200

    Responsive navbar fix

--- src/Views/admin/partials/javascripts.blade.php
+++ src/Views/admin/partials/javascripts.blade.php
@@ -4,6 +4,7 @@
 <script src="{{ url('quickadmin/js') }}/timepicker.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.js"></script>
 <script src="//cdn.ckeditor.com/4.5.4/full/ckeditor.js"></script>
+<script src="{{ url('quickadmin/js') }}/bootstrap.min.js"></script>
 <script src="{{ url('quickadmin/js') }}/main.js"></script>
 
 <script>
