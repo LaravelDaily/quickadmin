325c1cf05c4ec1b0e838678a686e884cb711988e
Skurygin Nikita <greattuz@gmail.com>
2016-03-11 14:06:23 -0500

    Russian language

---
+++ src/Translations/ru/admin.php
@@ -0,0 +1,55 @@
+<?php
+return [
+
+  // dashboard
+  'dashboard-title' => 'Добро пожаловать',
+
+  // partials-header
+  'partials-header-title' => 'QuickAdmin рус',
+
+  // partials-sidebar
+  'partials-sidebar-menu' => 'Меню',
+  'partials-sidebar-users' => 'Пользователи',
+  'partials-sidebar-user-actions' => 'Действия пользователей',
+  'partials-sidebar-logout' => 'Выйти',
+
+  // partials-topbar
+  'partials-topbar-title' => 'QuickAdmin рус',
+
+  // users-create
+  'users-create-create_user' => 'Создать пользователя',
+  'users-create-name' => 'Логин',
+  'users-create-name_placeholder' => 'Логин',
+  'users-create-email' => 'Email',
+  'users-create-email_placeholder' => 'Email',
+  'users-create-password' => 'Пароль',
+  'users-create-password_placeholder' => 'Пароль',
+  'users-create-role' => 'Роль',
+  'users-create-btncreate' => 'Создать',
+
+  // users-create
+  'users-edit-edit_user' => 'Редактировать пользователя',
+  'users-edit-name' => 'Логин',
+  'users-edit-name_placeholder' => 'Логин',
+  'users-edit-email' => 'Email',
+  'users-edit-email_placeholder' => 'Email',
+  'users-edit-password' => 'Пароль',
+  'users-edit-password_placeholder' => 'Пароль',
+  'users-edit-role' => 'Роль',
+  'users-edit-btnupdate' => 'Обновить',
+
+  // users-index
+  'users-index-add_new' => 'Создать',  
+  'users-index-users_list' => 'Список пользователей',
+  'users-index-name' => 'Логин',
+  'users-index-edit' => 'Редактировать',
+  'users-index-delete' => 'Удалить',
+  'users-index-are_you_sure' => 'Вы уверены?',
+  'users-index-no_entries_found' => 'Пользователь на заходил',
+
+  // users-controller
+  'users-controller-successfully_created' => 'Пользователь успешно создан!',
+  'users-controller-successfully_updated' => 'Пользователь успешно отредактирован!',  
+  'users-controller-successfully_deleted' => 'Пользователь успешно удален!',
+
+];
---
+++ src/Translations/ru/templates.php
@@ -0,0 +1,44 @@
+<?php
+return [
+
+  // templates-menu_field_line
+  'templates-menu_field_line-field_db_name' => 'Наименование поля в БД',
+  'templates-menu_field_line-size_limit' => 'Ограничение размера файла (в Мб):',
+  'templates-menu_field_line-size_limit_placeholder' => 'Ограничение размера файла (в Мб)',
+  'templates-menu_field_line-maximum_width' => 'Максимальная ширина:',
+  'templates-menu_field_line-maximum_width_placeholder' => 'Максимальная ширина',
+  'templates-menu_field_line-maximum_height' => 'Максимальная высота:',
+  'templates-menu_field_line-maximum_height_placeholder' => 'Максимальная высота',
+  'templates-menu_field_line-value' => 'Значение',
+  'templates-menu_field_line-dont_use_ckeditor' => 'Не использовать CKEDITOR',
+  'templates-menu_field_line-use_ckeditor' => 'Использовать CKEDITOR',
+  'templates-menu_field_line-select_relationship' => 'Выбрать связи',
+  'templates-menu_field_line-enum_values' => 'Перечисление значений: (разделитель - запятая)',
+  'templates-menu_field_line-enum_values_placeholder' => 'Перечисление значений: (разделитель - запятая)',
+  'templates-menu_field_line-field_visual_title_placeholder' => 'Поля заголовка',
+  'templates-menu_field_line-comment_below_placeholder' => 'Дополнительный комментарий к полю',
+
+  // templates-customView_index
+  'templates-customView_index-list' => 'Список',
+  'templates-customView_index-welcome_custom_view' => 'Добро пожаловать',
+
+  // templates-view_create
+  'templates-view_create-add_new' => 'Добавить новый',
+  'templates-view_create-create' => 'Создать',
+
+  // templates-view_create
+  'templates-view_edit-edit' => 'Редактировать',
+  'templates-view_edit-update' => 'Обновить',
+  'templates-view_edit-cancel' => 'Отмена',
+
+  // templates-view_index
+  'templates-view_index-add_new' => 'Добавить',
+  'templates-view_index-list' => 'Список',
+  'templates-view_index-edit' => 'Редактировать',
+  'templates-view_index-delete' => 'Удалить',
+  'templates-view_index-delete_checked' => 'Удалить отметку',
+  'templates-view_index-confirm_deletion2' => 'Действительно хотите удалить?',  
+  'templates-view_index-no_entries_found' => 'Ничего не найдено.',
+  'templates-view_index-are_you_sure' => 'Уверены?',
+
+];
