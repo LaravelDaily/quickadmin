b741f7fd996046fe08fa2b8c0a71be3656e9f141
ModestasV <info@modestasv.lt>
2016-04-04 14:35:00 +0300

    Fix: When editing user, his role was not setting correctly.

--- src/Views/admin/users/edit.blade.php
+++ src/Views/admin/users/edit.blade.php
@@ -44,7 +44,7 @@
     <div class="form-group">
         {!! Form::label('role_id', trans('quickadmin::admin.users-edit-role'), ['class'=>'col-sm-2 control-label']) !!}
         <div class="col-sm-10">
-            {!! Form::select('role_id', $roles, old('role_id', $user->role), ['class'=>'form-control']) !!}
+            {!! Form::select('role_id', $roles, old('role_id', $user->role_id), ['class'=>'form-control']) !!}
         </div>
     </div>
 
