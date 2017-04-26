a5cac6705764e3f19c568de9ce53cb721953aee4
Remaja Bioinformatik <qeifar@users.noreply.github.com>
2017-04-26 20:16:53 +0800

    Additional Lang
    Thank You for this great things. It really help me. As an appreciation I created a new translation for Bahasa which a major language at Southeast Asia :). Hope it can bring more developer from my region to use and buy your service.

---
+++ src/Translations/my/emails.php
@@ -0,0 +1,7 @@
+<?php
+return [
+
+  // emails-password
+  'password-reset_your_password' => 'Klik bagi menetapkan semula kata laluan anda:',
+
+];
---
+++ src/Translations/my/strings.php
@@ -0,0 +1,19 @@
+<?php
+return [
+
+  /*
+    Datatables Internationalisation - Available translations
+      https://www.datatables.net/plug-ins/i18n/
+  */
+  'datatable_url_language' => '//cdn.datatables.net/plug-ins/1.10.11/i18n/English.json',
+
+  // strings
+  'yes' => 'Ya',
+  'no' => 'Tidak',
+  'optional' => 'Tidak wajib',
+  'required' => 'Wajib diisi',
+  'required_unique' => 'Harus unik',
+  'default_unchecked' => 'Lalai; tidak bertanda',
+  'default_checked' => 'Lalai; bertanda'  
+  
+];
